e56de610fd6d7bb5499a710867627871e5197719
rizki siraj <jomirwa@gmail.com>
2026-02-06 21:20:44 +0700

    Initial Commit

---
+++ GithubAPIWrapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAPIWrapper
+//
+//  Created by Rizki Siraj on 06/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
